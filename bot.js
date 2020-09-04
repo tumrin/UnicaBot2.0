669b1bfe3466169d920523033998215d9469f9d2
Tuomas Rinne <tuomas.rin@hotmail.com>
2020-09-04 18:03:47 +0300

    works ok

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ const prefix = "!";
 const command = "ruoka";
 const fetch = require("node-fetch");
 let url = ""
-const settings = {method: "Get"};
-const ravintolat = ["assarin-ullakko"];
+const settings = { method: "Get" };
+const ravintolat = ["assarin-ullakko", "galilei", "macciavelli"];
 token = require("./token.json");
 var ruokaViesti = "";
 var response = "";
@@ -39,48 +39,89 @@ client.on("message", (message) => {
   const args = message.content.slice(prefix.length).split(/ +/);
   const commandName = args.shift().toLowerCase();
 
-  if(!command){
-      return;
+  if (!command) {
+    return;
   }
-  else{
-      message.reply(response);
+  else {
+    message.reply(response);
   }
 });
 
 try {
-    console.log("Päivitä");
-    paivitaRuoat();
-    annaRuoat();
+  console.log("Päivitä");
+  paivitaRuoat();
+  annaRuoat();
 } catch (error) {
-    console.error(error);
+  console.error(error);
 }
 
-function paivitaRuoat(){
-    url = "https://www.unica.fi/modules/json/json/Index?costNumber=1920&language=fi";
-    fetch(url, settings)
-    .then(res => res.json())
-    .then((json) => {
-        let data = JSON.stringify(json);
-        fs.writeFileSync("assari.json", data);
-    });
-    response = "";
+function paivitaRuoat() {
+  for (let i=0;i<ravintolat.length;i++){
+    switch (ravintolat[i]) {
+      case "assarin-ullakko":
+        url = "https://www.unica.fi/modules/json/json/Index?costNumber=1920&language=fi";
+        fetch(url, settings)
+          .then(res => res.json())
+          .then((json) => {
+            let data = JSON.stringify(json);
+            fs.writeFileSync("assari.json", data);
+          });
+        break;
+      case "galilei":
+        url = "https://www.unica.fi/modules/json/json/Index?costNumber=1995&language=fi";
+        fetch(url, settings)
+          .then(res => res.json())
+          .then((json) => {
+            let data = JSON.stringify(json);
+            fs.writeFileSync("galilei.json", data);
+          });
+        break;
+      case "macciavelli":
+        console.log("maccis päivitetty");
+        url = "https://www.unica.fi/modules/json/json/Index?costNumber=1970&language=fi";
+        fetch(url, settings)
+          .then(res => res.json())
+          .then((json) => {
+            let data = JSON.stringify(json);
+            fs.writeFileSync("maccis.json", data);
+          });
+        break;
+    }
+  }
+  response = "";
 }
 
-function annaRuoat(){
-    ruokaViesti = fs.readFileSync("assari.json",'utf-8');
-    var viesti = JSON.parse(ruokaViesti);
-    console.log(viesti.MenusForDays[1].SetMenus);
-    var vastaus = rakennaViesti(viesti);
-    return vastaus;
+function annaRuoat() {
+  for (let i=0;i<ravintolat.length;i++) {
+    switch (ravintolat[i]) {
+      case "assarin-ullakko":
+        console.log("asssari check");
+        ruokaViesti = fs.readFileSync("assari.json", 'utf-8');
+        var viesti = JSON.parse(ruokaViesti);
+        console.log(viesti.MenusForDays[1].SetMenus);
+        rakennaViesti(viesti);
+        break;
+      case "galilei":
+        ruokaViesti = fs.readFileSync("galilei.json", 'utf-8');
+        var viesti = JSON.parse(ruokaViesti);
+        console.log(viesti.MenusForDays[1].SetMenus);
+        rakennaViesti(viesti);
+        break;
+      case "macciavelli":
+        ruokaViesti = fs.readFileSync("maccis.json", 'utf-8');
+        var viesti = JSON.parse(ruokaViesti);
+        console.log(viesti.MenusForDays[1].SetMenus);
+        rakennaViesti(viesti);
+        break;
+    }
+  }
 }
 
-function rakennaViesti(viesti){
-    response += `Ravintola: ${viesti.RestaurantName}\n`
-      for(j in viesti.MenusForDays[0].SetMenus){
-        for(k in viesti.MenusForDays[0].SetMenus[j].Components){
-            response += JSON.stringify(viesti.MenusForDays[0].SetMenus[j].Components[k])+"\n";
-        }
-      }
-    `Ruuat:${JSON.stringify(viesti.MenusForDays[1].SetMenus[1].Components[0])}`
-    return response;
+function rakennaViesti(viesti) {
+  response += `\n**Ravintola: ${viesti.RestaurantName}  ${viesti.MenusForDays[0].Date}**\n`
+  for (j in viesti.MenusForDays[0].SetMenus) {
+    for (k in viesti.MenusForDays[0].SetMenus[j].Components) {
+      response += `> ${JSON.stringify(viesti.MenusForDays[0].SetMenus[j].Components[k])}"\n`;
+    }
+  }
 }
