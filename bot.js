ec78708c5d5c6048d6d058fe196281b93bd97444
Tuomas Rinne <tuomas.rin@hotmail.com>
2020-09-04 12:06:00 +0300

    fixed some errors

--- bot.js
+++ bot.js
@@ -4,14 +4,19 @@ const fs = require("fs");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
-
-token = require("token.json");
+const prefix = "!";
+const command = "ruoka";
+const fetch = require("node-fetch");
+let url = ""
+const settings = {method: "Get"};
+const ravintolat = ["assarin-ullakko"];
+token = require("./token.json");
 
 // triggered when bot:
 //  -logs in
 //  -reconnects after disconnecting
 client.on("ready", () => {
-  console.log(name + " is now logged in");
+  console.log("UnicaBot is now logged in");
   client.user.setPresence({
     status: "online",
     game: {
@@ -23,11 +28,8 @@ client.on("ready", () => {
 
 client.login(token["token"]);
 
-// triggered when bot receives message
+
 client.on("message", (message) => {
-  //ignore messages on other channels
-  //if (message.channel.name !== channel) return;
-  // ignore non-commands and own messages
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   console.log(`${message.author.username}:${message.content}`);
@@ -36,38 +38,36 @@ client.on("message", (message) => {
   const args = message.content.slice(prefix.length).split(/ +/);
   const commandName = args.shift().toLowerCase();
 
-  // check for command name in commands list, including aliases
-  const command =
-    client.commands.get(commandName) ||
-    client.commands.find(
-      (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
-    );
-
-  if (!command) return;
-
-  // if command requires args but none are provided
-  if (command.args == true && !args.length) {
-    let reply = `Tämä komento vaatii argumentteja, ${message.author}`;
-    if (command.usage) {
-      reply += `\nKomennon oikea käyttö on: \`${prefix}${command.name} ${command.usage}\``;
-    }
-    return message.channel.send(reply);
+  if(!command){
+      return;
   }
-
-  // try executing the command provided
-  try {
-    command.execute(message, args);
-  } catch (error) {
-    // catch errors when executing the command
-    console.error(error);
-    message.reply("jotain meni pieleen.");
+  else{
+      message.reply(ruokaViesti);
   }
-  console.log(message.author.username + ": " + message.content);
 });
 
-paivitaRuoat();
-
-setInterval(() => {
-  console.log("Trying to update");
+try {
     paivitaRuoat();
-}, 1000 * 60 * 60 * 4); // 4 tunnin välein
+} catch (error) {
+    console.error(error);
+}
+
+function paivitaRuoat(){
+    for(i in ravintolat){
+        switch(i){
+            case "assarin-ullakko":
+                url = "https://www.unica.fi/modules/json/json/Index?costNumber=1920&language=fi";
+                break;
+        default:
+            continue;
+        }
+        
+    fetch(url, settings)
+    .then(res => res.json())
+    .then((json) => {
+        let data = JSON.stringify(json);
+        let parsedData = {"ravintola": `${data["RestaurantName"]}`};
+        fs.writeFileSync("ruoat.json", parsedData);
+    });
+    }
+}
